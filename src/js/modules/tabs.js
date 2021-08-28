f93e65ed6b94adfbaee89309bccbb3b4e3cec722
Taras Slovak <slovak.taras@gmail.com>
2021-08-28 18:28:33 +0300

    TABS

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,4 +1,4 @@
-
+//TABS
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     const header = document.querySelector(headerSelector),
         tab = document.querySelectorAll(tabSelector),
